9e86a96e6b2df179d37b61f3a53b0676a2ca0e52
flanflanagan <flanny65@gmail.com>
2013-08-29 13:38:00 -0500

    prep for facilityNode refactor

--- cnome/src/edu/utexas/cycic/cycicScenarios.java
+++ cnome/src/edu/utexas/cycic/cycicScenarios.java
@@ -3,6 +3,11 @@ package edu.utexas.cycic;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * Built to manage scenarios.
+ * @author Robert
+ *
+ */
 public class CycicScenarios implements Serializable{
 	static ArrayList<DataArrays> cycicScenarios = new ArrayList<DataArrays>();
 	static DataArrays workingCycicScenario;
