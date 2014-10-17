11e0bb644e4a463cdbc76d2ff5494d4cf71fdbd6
flanflanagan <flanny65@gmail.com>
2014-10-17 15:37:18 -0500

    new lib

--- deploy/build/src/edu/utexas/cycic/CycicNotifications.java
+++
@@ -1,7 +0,0 @@
-package edu.utexas.cycic;
-
-public class CycicNotifications {
-
-	public static final String NEW_INSTIT 	= "new_instit"; 
-	
-}
--- deploy/project/src/edu/utah/sci/cyclist/neup/model/Inventory.java
+++
@@ -1,7 +0,0 @@
-package edu.utah.sci.cyclist.neup.model;
-
-public class Inventory {
-	public int time;
-	public int nucid;
-	public double amount;
-}
