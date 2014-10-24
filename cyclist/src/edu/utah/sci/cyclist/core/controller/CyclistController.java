1671b45e66d6d52e1a729b4125e72683b9a60596
hayagur <hayagur@sci.utah.edu>
2014-10-24 14:39:30 -0600

    Save As - start with the parent directory.

--- cyclist/src/edu/utah/sci/cyclist/core/controller/CyclistController.java
+++ cyclist/src/edu/utah/sci/cyclist/core/controller/CyclistController.java
@@ -45,6 +45,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.MenuItem;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -406,7 +407,13 @@ public class CyclistController {
 			public void handle(ActionEvent event) {
 				FileChooser chooser = new FileChooser();
 				chooser.setInitialDirectory(new File(getLastChosenWorkDirectory()+"/.."));
+//				DirectoryChooser chooser = new DirectoryChooser();
+				File dir = new File(getLastChosenWorkDirectory());
+				String parent = dir.getParent();
+				chooser.setInitialDirectory(new File(parent));
+//				chooser.getExtensionFilters().add( new FileChooser.ExtensionFilter("directories", "*") );
 				File file = chooser.showSaveDialog(Cyclist.cyclistStage);
+//				File file = chooser.showDialog(Cyclist.cyclistStage);
 				if (file != null) {
 					saveAs(file.getAbsolutePath());
 					//Display the new directory in the work directories dialog.
